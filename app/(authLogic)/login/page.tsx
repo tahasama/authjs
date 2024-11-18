27e3777b127629a9a4ae808b409653df501fe261
tahasama <taha.maatof@gmail.com>
2024-11-18 18:16:08 +0100

    added register and login ui

---
+++ app/(authLogic)/login/page.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="grid items-center justify-items-center min-h-screen">
+      <main className="grid gap-10 p-4 rounded ring-2 ring-purple-950">
+        <h1 className="text-center">Login page</h1>
+        <form action="" className="flex flex-col gap-3">
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            name="email"
+            className="rounded p-1.5 bg-slate-500 text-slate-300"
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="text"
+            name="password"
+            className="rounded p-1.5 bg-slate-500 text-slate-300"
+          />
+          <button
+            type="submit"
+            className="bg-blue-950 text-center rounded-md p-1.5 mt-1 hover:bg-blue-900/50"
+          >
+            Login
+          </button>
+        </form>
+      </main>
+    </div>
+  );
+};
+
+export default Login;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>this is a Auth app</h1>
-      <h2>we will create an all auth app with auth.js</h2>
-      <p>we will show here if user is logged or not</p>
+    <div className="grid items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <main className="grid gap-10">
+        <h1 className="text-xl">This is a Auth app</h1>
+        <h2 className="text-lg">We will create an all auth app with auth.js</h2>
+        <p>We will show here if user is logged or not</p>
+        <div className="p-5 bg-purple-950 rounded-md">Not user yet!</div>
+        <Link
+          href={"/login"}
+          className="bg-slate-900 rounded-md p-3 flex justify-between max-w-52"
+        >
+          got to login page
+          <span>--{`>`}</span>
+        </Link>
+        <Link
+          href={"/register"}
+          className="bg-blue-950 rounded-md p-3 flex justify-between max-w-52"
+        >
+          got to register page
+          <span>--{`>`}</span>
+        </Link>
+      </main>
     </div>
   );
 }
