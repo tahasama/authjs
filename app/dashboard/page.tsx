3a54d3d4f38ac1120bdba32cdf04f978259fac5c
tahasama <taha.maatof@gmail.com>
2024-11-19 22:24:25 +0100

    reset password

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,24 +1,38 @@
 "use client";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
-import React from "react";
 
 const Dashboard = () => {
   const { status, data } = useSession();
+
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center min-h-screen">
       {status === "authenticated" ? (
-        <>
-          <h2>
-            {data.user?.email}
-            {`'`}s Dashboard
+        <div>
+          <h2 className="text-xl font-semibold mb-4">
+            Welcome, <span className="text-blue-600">{data.user?.email}</span>!
           </h2>
-          <Link href="/chngPsswrd">reset password</Link>
-        </>
+          <p className="text-gray-600 mb-8">This is your dashboard.</p>
+          <Link
+            href="/chngPsswrd"
+            className=" px-6 py-3 text-sm font-medium text-white bg-blue-500 rounded-lg hover:bg-blue-600"
+          >
+            Reset Password
+          </Link>
+        </div>
       ) : (
-        <>
-          <Link href={"/login"}>Login</Link>
-        </>
+        <div>
+          <h2 className="text-xl font-semibold mb-4">You are not logged in</h2>
+          <p className="text-gray-600 mb-8">
+            Please log in to access your dashboard.
+          </p>
+          <Link
+            href="/login"
+            className="px-6 py-3 text-sm font-medium text-white bg-blue-500 rounded-lg hover:bg-blue-600"
+          >
+            Login
+          </Link>
+        </div>
       )}
     </div>
   );
