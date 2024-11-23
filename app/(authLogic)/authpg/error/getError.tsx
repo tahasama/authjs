97a2f97b798bd9be25c98caa19686e5293d06c14
tahasama <taha.maatof@gmail.com>
2024-11-23 22:46:54 +0100

    added real domai + magic link + custom pages

---
+++ app/(authLogic)/authpg/error/getError.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { ErrorMessagesProps } from "@/lib/types";
+import { useSearchParams } from "next/navigation";
+
+const GetError = () => {
+  const searchParams = useSearchParams();
+  const error: string | null = searchParams.get("error");
+
+  const errorMessages: ErrorMessagesProps = {
+    Configuration: "There was a problem with the configuration.",
+    AccessDenied: "You don't have permission to access this.",
+    Default: "Something went wrong. Please try again later.",
+    Verification:
+      "Unable to sign in The sign in link is no longer valid.It may have been used already or it may have expired.",
+  };
+
+  //   const errorMessage = error && errorMessages[error] ? errorMessages[error] : errorMessages.Default;
+  const errorMessage: string = error
+    ? errorMessages[error as keyof ErrorMessagesProps]
+    : errorMessages.Default;
+
+  return <div>{error ? errorMessage : errorMessages.Default}</div>;
+};
+
+export default GetError;
--- app/(authLogic)/authpg/error/page.tsx
+++ app/(authLogic)/authpg/error/page.tsx
@@ -1,27 +1,12 @@
-"use client";
-import { ErrorMessagesProps } from "@/lib/types";
-import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { Suspense } from "react";
+import GetError from "./getError";
 
 const LogError = () => {
-  const searchParams = useSearchParams();
-  const error: string | null = searchParams.get("error");
-  console.log("🚀 ~ LogError ~ error:", error);
-
-  const errorMessages: ErrorMessagesProps = {
-    Configuration: "There was a problem with the configuration.",
-    AccessDenied: "You don't have permission to access this.",
-    Default: "Something went wrong. Please try again later.",
-    Verification:
-      "Unable to sign in The sign in link is no longer valid.It may have been used already or it may have expired.",
-  };
-
-  //   const errorMessage = error && errorMessages[error] ? errorMessages[error] : errorMessages.Default;
-  const errorMessage: string = error
-    ? errorMessages[error as keyof ErrorMessagesProps]
-    : errorMessages.Default;
-
-  return <div>{error ? errorMessage : errorMessages.Default}</div>;
+  return (
+    <Suspense fallback={"Loading..."}>
+      <GetError />
+    </Suspense>
+  );
 };
 
 export default LogError;
