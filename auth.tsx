7f60ebd403863f885c4d4b7dd8a4bc9ebe6e1a39
tahasama <taha.maatof@gmail.com>
2024-11-24 21:59:20 +0100

    fix missing psswrdhash from social login

--- auth.tsx
+++ auth.tsx
@@ -8,6 +8,8 @@ import { getUserFromDb } from "./app/actions/authActions";
 import authConfig from "./auth.config";
 import PostgresAdapter from "@auth/pg-adapter";
 import { pool } from "./lib/db";
+import { saltAndHashPassword } from "./lib/utils";
+import { v4 as uuidv4 } from "uuid";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   ...authConfig,
@@ -30,7 +32,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         psswrd: {},
       },
       async authorize(credentials: any) {
-        console.log("000000000000");
         const { email, password } = await loginSchema.parseAsync(credentials);
         const result = await getUserFromDb(email, password);
         console.log("🚀 ~ authorize ~ result:", result.user);
@@ -51,5 +52,35 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   },
   session: {
     strategy: "jwt", // Use JWT or database-based sessions
+  },
+  callbacks: {
+    async signIn({ user, account }) {
+      if (
+        account &&
+        (account.provider === "google" || account.provider === "github")
+      ) {
+        try {
+          // Check if user already exists in the database
+          const existingUser = await pool.query(
+            "SELECT * FROM users WHERE email = $1",
+            [user.email]
+          );
+
+          if (!existingUser.rows.length) {
+            const Verycomplicatedword = uuidv4();
+            // User doesn't exist, insert them with a placeholder psswrdhash
+            const placeholderHash = saltAndHashPassword(Verycomplicatedword);
+            await pool.query(
+              "INSERT INTO users (email, psswrdhash) VALUES ($1, $2)",
+              [user.email, placeholderHash]
+            );
+          }
+        } catch (err) {
+          console.error("Error in signIn callback:", err);
+          return false; // Prevent sign-in if something goes wrong
+        }
+      }
+      return true;
+    },
   },
 });
