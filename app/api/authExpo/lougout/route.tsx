2c15c881bbcb5d9a519c164a9fb46f6cee2c797b
tahasama <taha.maatof@gmail.com>
2024-11-28 17:05:29 +0100

    added experimental api logout

--- app/api/authExpo/lougout/route.tsx
+++ app/api/authExpo/lougout/route.tsx
@@ -4,7 +4,10 @@ import { NextResponse } from "next/server";
 
 export async function POST() {
   try {
+    console.log("🚀 ~ AAAA ~ 111111111111:");
     await signOut(); // Ensure signOut() performs the logout correctly.
+    console.log("🚀 ~ BBBB ~ 222222222222:");
+
     return NextResponse.json({ message: "Logged out successfully" });
   } catch (error) {
     console.error("Logout failed:", error);
