2a9d9c21d46f906270c27503ef7169d1d96abeea
tahasama <taha.maatof@gmail.com>
2024-11-28 16:39:37 +0100

    added experimental api logout

---
+++ app/api/authExpo/lougout/route.tsx
@@ -0,0 +1,20 @@
+import { logout } from "@/app/actions/authActions";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    // This endpoint handles logout requests.
+    // No need to verify the token here; just provide a response indicating logout success.
+
+    // If you are managing server-side sessions, clear them here.
+    // For example: await clearSessionForUser(token.userId);
+    await logout();
+    return NextResponse.json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.error("Logout failed:", error);
+    return NextResponse.json(
+      { error: "Something went wrong during logout" },
+      { status: 500 }
+    );
+  }
+}
